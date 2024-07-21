5ec56348bee2b51b79c76883198a45b1dc3fb535
z <iamzanieltan@gmail.com>
2024-07-21 17:38:09 +0800

    test

--- src/main/java/com/example/corebankingapplication/model/Customer.java
+++ src/main/java/com/example/corebankingapplication/model/Customer.java
@@ -1,5 +1,69 @@
 package com.example.corebankingapplication.model;
 
-public class Customer {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private int mobileNo;
+
+    public Customer(long id, String name, String email, int mobileNo) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobileNo = mobileNo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String Name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getmobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer " +
+                ", id = " + id +
+                ", name = " + name +
+                ", email = " + email +
+                ", mobile = " + mobileNo;
+    }
 }
