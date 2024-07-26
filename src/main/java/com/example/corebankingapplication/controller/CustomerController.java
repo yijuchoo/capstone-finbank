892c2d0c982227a730120aab9737c947ffe3bac4
Yirou C. <yirou26@msn.com>
2024-07-27 02:00:21 +0800

    Apply bootstrap styles to form and tables

--- src/main/java/com/example/corebankingapplication/controller/CustomerController.java
+++ src/main/java/com/example/corebankingapplication/controller/CustomerController.java
@@ -1,14 +1,28 @@
 package com.example.corebankingapplication.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.corebankingapplication.repo.AccountRepository;
+import com.example.corebankingapplication.model.Customer;
+import com.example.corebankingapplication.repo.CustomerRepository;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
     @Autowired
-    AccountRepository accountRepository;
+    CustomerRepository customerRepository;
+
+    @RequestMapping("/list")
+    public String showCust(Model model, Principal principal) {
+        List<Customer> custList = customerRepository.findAll();
+        model.addAttribute("customers", custList);
+        return "viewcustomers";
+    }
     
 }
