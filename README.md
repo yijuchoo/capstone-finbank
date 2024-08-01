91ee3410f1dabfd6ae3515e91bf68d25b43e21a4
Yirou C. <yirou26@msn.com>
2024-08-02 01:09:36 +0800

    Remove all files from the branch

--- README.md
+++
@@ -1,7 +0,0 @@
-# capstone
-<<<<<<< HEAD
-
-use the following to clone
-"git clone https://github.com/ayamzan/capstone.git"
-=======
->>>>>>> main
--- src/main/java/com/example/corebankingapplication/repo/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.corebankingapplication.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.corebankingapplication.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
