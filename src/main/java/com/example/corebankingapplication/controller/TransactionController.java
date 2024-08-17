5c6d3469120734a0a4e6d427de4cacf8270fad00
Yirou C. <yirou26@msn.com>
2024-08-18 00:19:04 +0800

    Update styles for responsive

--- src/main/java/com/example/corebankingapplication/controller/TransactionController.java
+++ src/main/java/com/example/corebankingapplication/controller/TransactionController.java
@@ -2,12 +2,10 @@ package com.example.corebankingapplication.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
